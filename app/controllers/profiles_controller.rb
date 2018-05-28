e8ab74335c3a8291487dc9797891d37b1eef987d
susott <37674058+susott@users.noreply.github.com>
2018-05-28 18:09:14 +0200

    advanced searc is working now

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,9 @@ class ProfilesController < ApplicationController
       basic_search
       gender_search
       age_search
+      dedication_search
+      name_search
+      keyword_search
     else
       @users = User.all
     end
@@ -48,6 +51,22 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def basic_search
+    # search for users, where params[:native] == current_users learning language
+    # filter for users, where params[:learning] == current_users native language
+    # filter for users around that city (10km?)
+
+    @native_users = User.joins(languages: :language_skills)
+          .where(languages: {name: params[:native]})
+          .where('language_skills.score < 6')
+    @learning_users = User.joins(languages: :language_skills)
+          .where(languages: {name: params[:learning]})
+          .where('language_skills.score = 6')
+    #@users_nearby = User.near([current_user.latitude,current_user.longitude],20)
+    @users_nearby = User.near(params[:city],20)
+    @users = @native_users & @learning_users & @users_nearby
+  end
+
   def age_search
     if params[:min_age].present? && params[:max_age].present?
         @users = @users.select do |user|
@@ -68,6 +87,36 @@ class ProfilesController < ApplicationController
     others = @users.select { |user| user.gender == "other"} if params[:other]
     if params[:male] || params[:female] || params[:other]
       @users = (males.to_a + females.to_a + others.to_a).uniq
+    end
+  end
+
+  def dedication_search
+    # ["occassionally", "once per month", "once per week", "more often"]
+    if params[:dedication] != "all"
+      @users = @users.select do |user|
+        user.dedication == params[:dedication]
+      end
+    end
+  end
+
+  def name_search
+    if params[:name].present?
+      @users = @users.select do |user|
+        user.name.downcase == params[:name].downcase
+      end
+    end
+  end
+
+  def keyword_search
+    if params[:keywords].present?
+      @users = @users.select do |user|
+        keywords = params[:keywords].split(" ")
+        keywords.any? do |keyword|
+          user.description.index( / #{keyword} /i)
+        end
+        #keyword = params[:keywords]
+        #user.description.index(/#{keyword}/i)
+      end
     end
   end
 
@@ -93,21 +142,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  def basic_search
-    # search for users, where params[:native] == current_users learning language
-    # filter for users, where params[:learning] == current_users native language
-    # filter for users around that city (10km?)
-
-    @native_users = User.joins(languages: :language_skills)
-          .where(languages: {name: params[:native]})
-          .where('language_skills.score < 6')
-    @learning_users = User.joins(languages: :language_skills)
-          .where(languages: {name: params[:learning]})
-          .where('language_skills.score = 6')
-    #@users_nearby = User.near([current_user.latitude,current_user.longitude],20)
-    @users_nearby = User.near(params[:city],20)
-    @users = @native_users & @learning_users & @users_nearby
-  end
 
   def geo_options
   #@users_geolocation = User.where.not(latitude: nil, longitude: nil)
