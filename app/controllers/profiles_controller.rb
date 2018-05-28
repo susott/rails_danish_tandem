43d35171cabe137148dae311c383a6e681ee107b
susott <37674058+susott@users.noreply.github.com>
2018-05-28 17:00:00 +0200

    profile controller refactored

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,79 +1,16 @@
 class ProfilesController < ApplicationController
   def index
-
-  #### default fill for search fields
+    default_field_fill
     if params[:native].present?
-      @native = params[:native]
-    else
-      @native = current_user.languages.first.name
-    end
-
-    if params[:learning].present?
-      @learning = params[:learning]
-    elsif current_user.languages.find_by('language_skills.score < 5')
-      @learning = current_user.languages.find_by('language_skills.score < 5').name
-    else
-      @learning = ""
-    end
-
-    if params[:city].present?
-      @city = params[:city]
-    else
-      @city = current_user.address
-    end
-  ####
-
-    if params[:native].present?
-      # search for users, where params[:native] == current_users learning language
-      # filter for users, where params[:learning] == current_users native language
-      # filter for users around that city (10km?)
-      @native_users = User.joins(languages: :language_skills)
-            .where(languages: {name: params[:native]})
-            .where('language_skills.score < 6')
-      @learning_users = User.joins(languages: :language_skills)
-            .where(languages: {name: params[:learning]})
-            .where('language_skills.score = 6')
-      #@users_nearby = User.near([current_user.latitude,current_user.longitude],20)
-      @users_nearby = User.near(params[:city],20)
-      @users = @native_users & @learning_users & @users_nearby
-
-      # advanced search options
-
-      # males = @users.where(gender: 'male') if params[:male]
-      # females = @users.where(gender: 'female') if params[:female]
-      # others = @users.where(gender: 'other') if params[:other]
-
-      # array of hashes
-      males = @users.select { |user| user.gender == "male"} if params[:male]
-      females = @users.select { |user| user.gender == "female"} if params[:female]
-      others = @users.select { |user| user.gender == "other"} if params[:other]
-
-      if params[:male] || params[:female] || params[:other]
-        @users = (males.to_a + females.to_a + others.to_a).uniq
-      end
-
-
-
-
-
-
+      basic_search
+      gender_search
+      age_search
     else
       @users = User.all
     end
-
-  #@users_geolocation = User.where.not(latitude: nil, longitude: nil)
-    @users_have_geo = User.joins(languages: :language_skills)
-          .where.not(latitude: nil, longitude: nil)
-    @users_geolocation = @users_have_geo & @users
-
-    @markers = @users_geolocation.map do |user|
-      {
-        lat: user.latitude,
-        lng: user.longitude,
-        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-      }
-    end
+    geo_options
   end
+
 
   def show
     @user = User.find(params[:id])
@@ -108,6 +45,82 @@ class ProfilesController < ApplicationController
         lng: @user.longitude
         # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
       }]
+    end
+  end
+
+  def age_search
+    if params[:min_age].present? && params[:max_age].present?
+        @users = @users.select do |user|
+          (user.age >= params[:min_age].to_i) && (user.age <= params[:max_age].to_i)
+        end
+
+    elsif params[:min_age].present?
+        @users = @users.select { |user| user.age >= params[:min_age].to_i}
+
+    elsif params[:max_age].present?
+        @users = @users.select { |user| user.age <= params[:max_age].to_i}
+    end
+  end
+
+  def gender_search
+    males = @users.select { |user| user.gender == "male"} if params[:male]
+    females = @users.select { |user| user.gender == "female"} if params[:female]
+    others = @users.select { |user| user.gender == "other"} if params[:other]
+    if params[:male] || params[:female] || params[:other]
+      @users = (males.to_a + females.to_a + others.to_a).uniq
+    end
+  end
+
+  def default_field_fill
+    if params[:native].present?
+      @native = params[:native]
+    else
+      @native = current_user.languages.first.name
+    end
+
+    if params[:learning].present?
+      @learning = params[:learning]
+    elsif current_user.languages.find_by('language_skills.score < 5')
+      @learning = current_user.languages.find_by('language_skills.score < 5').name
+    else
+      @learning = ""
+    end
+
+    if params[:city].present?
+      @city = params[:city]
+    else
+      @city = current_user.address
+    end
+  end
+
+  def basic_search
+    # search for users, where params[:native] == current_users learning language
+    # filter for users, where params[:learning] == current_users native language
+    # filter for users around that city (10km?)
+
+    @native_users = User.joins(languages: :language_skills)
+          .where(languages: {name: params[:native]})
+          .where('language_skills.score < 6')
+    @learning_users = User.joins(languages: :language_skills)
+          .where(languages: {name: params[:learning]})
+          .where('language_skills.score = 6')
+    #@users_nearby = User.near([current_user.latitude,current_user.longitude],20)
+    @users_nearby = User.near(params[:city],20)
+    @users = @native_users & @learning_users & @users_nearby
+  end
+
+  def geo_options
+  #@users_geolocation = User.where.not(latitude: nil, longitude: nil)
+    @users_have_geo = User.joins(languages: :language_skills)
+          .where.not(latitude: nil, longitude: nil)
+    @users_geolocation = @users_have_geo & @users
+
+    @markers = @users_geolocation.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
     end
   end
 end
