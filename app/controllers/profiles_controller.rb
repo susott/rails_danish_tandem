e3aec1a992aca0e7efb69aea8b57b4592d473b6f
eugenia cadavid <eumar-8@hotmail.com>
2018-05-30 11:59:22 +0200

    some changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,6 +18,7 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
     define_profile
+
   end
 
   def update
@@ -29,6 +30,10 @@ class ProfilesController < ApplicationController
   def my_dashboard
     @user = current_user
     define_profile
+    @my_conversations = User.joins(:received_messages).joins(:sent_messages)
+                  .where("messages.sender_id = #{current_user.id} OR
+                   messages.receiver_id = #{current_user.id}")
+                  .distinct.reject { |user| user ==current_user }
   end
 
   def profile_update_params
