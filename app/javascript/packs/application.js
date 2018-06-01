50f902e42fcc884d17da47df048eb3ff95b103e7
eugenia cadavid <eumar-8@hotmail.com>
2018-06-01 13:43:11 +0200

    home fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
 import "bootstrap";
 import "./map.js";
 import { searchfun } from "../components/searchfun.js";
+import { loadDynamicBannerText } from '../components/type';
+loadDynamicBannerText();
 
 searchfun();
+
